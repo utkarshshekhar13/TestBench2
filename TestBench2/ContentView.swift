ebbb8e4619aead3e0fa7e3840834999726ebeba3
utkarshshekhar13 <utki.619@gmail.com>
2019-11-25 21:23:51 +0530

    Initial Commit

---
+++ TestBench2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestBench2
+//
+//  Created by Utkarsh Shekhar on 25/11/19.
+//  Copyright © 2019 Utkarsh Shekhar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
